43ccf2ef9b1200a6a85882e5330e97bededa6fd3
jklimek <jklimek@fidolabs.com>
2015-12-21 02:26:12 +0100

    Fixed viewport vars

--- public_html/js/game.js
+++ public_html/js/game.js
@@ -2,9 +2,6 @@
  * Created by klemens on 18/12/15.
  */
 
-
-var cWidth = 1000, cHeight = 600;
-
 var c = document.getElementById("c");
 var ctx = c.getContext("2d");
 
@@ -27,12 +24,12 @@ var drawHud = function (points, level) {
 // Paddle section
 var paddleWidth = 100;
 var paddleHeight = 10;
-var paddleX = cWidth / 2 - paddleWidth / 2;
-var paddleY = cHeight - paddleHeight;
+var paddleX = c.width / 2 - paddleWidth / 2;
+var paddleY = c.height - paddleHeight;
 
 // Ball section
-var ballX = cWidth / 2;
-var ballY = cHeight - 10;
+var ballX = c.width / 2;
+var ballY = c.height - 10;
 var ballDX = getRandomArbitrary(-5, 5);
 var ballDY = getRandomArbitrary(-5, -4);
 var ballRadius = 7;
@@ -102,20 +99,20 @@ document.addEventListener("keyup", keyUpHandler, false);
 
 
 var gameLoop = function () {
-    ctx.clearRect(0, 0, cWidth, cHeight);
+    ctx.clearRect(0, 0, c.width, c.height);
     ball(ballX, ballY);
-    if (ballX + ballDX > cWidth - ballRadius || ballX + ballDX < ballRadius) {
+    if (ballX + ballDX > c.width - ballRadius || ballX + ballDX < ballRadius) {
         ballDX = -ballDX;
     }
     if (ballY + ballDY < ballRadius) {
         ballDY = -ballDY;
     }
 
-    if (ballY > cHeight) {
+    if (ballY > c.height) {
         ballOut = true;
     }
 
-    if (rightPressed && paddleX < cWidth - paddleWidth) {
+    if (rightPressed && paddleX < c.width - paddleWidth) {
         paddleX += 7;
     } else if (leftPressed && paddleX > 0) {
         paddleX -= 7;
@@ -177,8 +174,8 @@ var loosingScreen = function () {
     ctx.font = "40px Arial";
     ctx.fillStyle = "#FF0000";
     ctx.textAlign = "center";
-    ctx.fillText("NEXT TIME, GADGET!", cWidth / 2, cHeight / 2);
-    ctx.fillText("PRESS R TO RESTART", cWidth / 2, cHeight / 2 + 100);
+    ctx.fillText("NEXT TIME, GADGET!", c.width / 2, c.height / 2);
+    ctx.fillText("PRESS R TO RESTART", c.width / 2, c.height / 2 + 100);
 };
 
 var ball = function (x, y) {
@@ -241,8 +238,8 @@ function getRandomArbitrary(min, max) {
 }
 var startGame = function () {
     inGame = 1;
-    ballX = cWidth / 2;
-    ballY = cHeight - 10;
+    ballX = c.width / 2;
+    ballY = c.height - 10;
     ballDX = getRandomArbitrary(-5, 5);
     ballDY = getRandomArbitrary(-5, -4);
     clearInterval(intervalId);
@@ -252,12 +249,12 @@ var startGame = function () {
 var restart = function () {
     points = 0;
     ballOut = false;
-    ballX = cWidth / 2;
-    ballY = cHeight - 11;
+    ballX = c.width / 2;
+    ballY = c.height - 11;
     ballDX = getRandomArbitrary(-5, 5);
     ballDY = getRandomArbitrary(-5, -4);
-    paddleX = cWidth / 2 - paddleWidth / 2;
-    paddleY = cHeight - paddleHeight;
+    paddleX = c.width / 2 - paddleWidth / 2;
+    paddleY = c.height - paddleHeight;
     blocksArray = JSON.parse(JSON.stringify(blocksArrayLvl1));
 
     clearInterval(intervalId);
@@ -265,20 +262,20 @@ var restart = function () {
 
 };
 
-var splashTitleX = cWidth / 2;
+var splashTitleX = c.width / 2;
 var splashLoop = function () {
     ctx.clearRect(0, 0, c.width, c.height);
     ctx.font = "40px Arial";
     ctx.textAlign = "center";
     ctx.fillStyle = "#FF0000";
-    ctx.fillText("BEST ARKANOID GAME", splashTitleX + getRandomArbitrary(-5, 5), cHeight / 2);
-    ctx.fillText("(PLAY SPACE TO PRESS)", splashTitleX + getRandomArbitrary(-5, 5), cHeight / 2 + 100);
+    ctx.fillText("BEST ARKANOID GAME", splashTitleX + getRandomArbitrary(-5, 5), c.height / 2);
+    ctx.fillText("(PLAY SPACE TO PRESS)", splashTitleX + getRandomArbitrary(-5, 5), c.height / 2 + 100);
 
     ball(ballX, ballY);
-    if (ballX + ballDX > cWidth - ballRadius || ballX + ballDX < ballRadius) {
+    if (ballX + ballDX > c.width - ballRadius || ballX + ballDX < ballRadius) {
         ballDX = -ballDX;
     }
-    if (ballY + ballDY > cHeight - ballRadius || ballY + ballDY < ballRadius) {
+    if (ballY + ballDY > c.height - ballRadius || ballY + ballDY < ballRadius) {
         ballDY = -ballDY;
     }
 
